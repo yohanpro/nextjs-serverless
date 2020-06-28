4000e6a1021cd6204ecc3140be60dc6588bde853
Yohan <yohan@yohanpro.com>
2020-06-28 10:24:43 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+// const Index = () => {
+//   return (
+//     <h1>I am Index Page</h1>
+//   )
+// }
+
+class Index extends React.Component {
+  render() {
+    return (
+      <h1>I am Index Page</h1>
+    )
+  }
+}
+
+export default Index;
